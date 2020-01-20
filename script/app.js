1ed43f00771a7fe8e9a6770e56a1d743f25c28aa
LittleBlue418 <hollyathomas88@gmail.com>
2020-01-20 10:38:45 +0100

    error for no number added

--- script/app.js
+++ script/app.js
@@ -4,31 +4,35 @@ function getJokes(e) {
   e.preventDefault()
   const number = document.querySelector('input[type="number"]').value;
 
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+  if (number === '') {
+    document.querySelector('.jokes').innerHTML = `Please enter a valid number`
+  } else {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
 
-  xhr.onload = function() {
-    if(this.status === 200) {
-      const response = JSON.parse(this.responseText);
+    xhr.onload = function () {
+      if (this.status === 200) {
+        const response = JSON.parse(this.responseText);
 
-      let output = '';
+        let output = '';
 
-      if(response.type === 'success') {
-        /*  NB we first target the value, thisis due to how the array
-            is set up. each array is different so check how your
-            response comes back! */
-        output += `<h3>Jokes</h3>`
+        if (response.type === 'success') {
+          /*  NB we first target the value, thisis due to how the array
+              is set up. each array is different so check how your
+              response comes back! */
+          output += `<h3>Jokes</h3>`
 
-        response.value.forEach(function(joke){
-          output += `<li>${joke.joke}</li>`
-        })
-      } else {
-        output += '<li>Something went wrong!</li>'
+          response.value.forEach(function (joke) {
+            output += `<li>${joke.joke}</li>`
+          })
+        } else {
+          output += '<li>Something went wrong!</li>'
+        }
+
+        document.querySelector('.jokes').innerHTML = output
       }
-
-      document.querySelector('.jokes').innerHTML = output
     }
-  }
 
-  xhr.send();
+    xhr.send();
+  }
 }
